b8045386cb227686cd25714e5785431ea2fca879
Bang <bangthvu@hotmail.com>
2022-05-17 15:21:15 +0200

    webpack update argv

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports = (_, argv) => ({
   output: {
     publicPath: 
     argv.mode === 'development' 
@@ -72,4 +72,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
